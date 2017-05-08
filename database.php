d89b9a50b81ae2103f0f44367168b23820ccc8b2
Simon Lee <simonhlee97@hotmail.com>
2017-05-08 12:07:58 +0000

    db connection

--- database.php
+++ database.php
@@ -1,18 +1,4 @@
-/*<?php
-// used to connect to the database
-$host = "127.0.0.1";
-$db_name = "ninja";
-$username = "simonhlee97";
-$password = "";
-$port = 3306;
-try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name};port={$port}", $username, $password);
-}
-// show error
-catch(PDOException $exception){
-    echo "Connection error: " . $exception->getMessage();
-}
-?>*/
+
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
