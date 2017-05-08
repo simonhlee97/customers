04263559609b6c96c2aaf001a9dd364de27e6806
Simon Lee <simonhlee97@hotmail.com>
2017-05-08 12:00:45 +0000

    configged ClearDB add-on from Heroku

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
-<?php
+/*<?php
 // used to connect to the database
 $host = "127.0.0.1";
 $db_name = "ninja";
@@ -6,10 +6,20 @@ $username = "simonhlee97";
 $password = "";
 $port = 3306;
 try {
-    $con = new PDO('mysql:host={$host};dbname={$db_name};port={$port}', $username, $password);
+    $con = new PDO("mysql:host={$host};dbname={$db_name};port={$port}", $username, $password);
 }
 // show error
 catch(PDOException $exception){
     echo "Connection error: " . $exception->getMessage();
 }
+?>*/
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-03.cleardb.net"];
+$username = $url["b5458e0da66d5b"];
+$password = $url["cba72f57"];
+$db = substr($url["heroku_f131178ef9fabf6"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
 ?>
